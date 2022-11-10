122533b91089557b701b53cedd4c62c819af4952
BruceLLDD <1049825131@qq.com>
2022-11-10 00:14:54 +0000

    implement distance_to()

--- cities.py
+++ cities.py
@@ -2,6 +2,7 @@ from typing import Dict, List, Tuple
 import math
 
 
+
 class City:
     def __init__(self, name: str, country: str, citizens: int,
                  latitude: float, longitude: float):
@@ -23,11 +24,11 @@ class City:
 
     # calculate the distance  from a city to another
     def distance_to(self, other: 'City') -> float:
-        city2 = 'City'
+        Buenos_Aires = 'City'
         lati1 = self.latitude
-        lati2 = city2.latitude
+        lati2 = other.latitude
         long1 = self.longitude
-        long2 = city2.longitude
+        long2 = other.latitude
         R = 6371
         distance = 2 * R * math.asin(math.sqrt(math.pow(math.sin((lati2 - lati1) / 2), 2) +
                                                math.cos(lati1) * math.cos(lati2) *
@@ -35,18 +36,22 @@ class City:
         return distance
 
     def co2_to(self, other: 'City') -> float:
-        raise NotImplementedError
+        self.distance_to(Buenos_Aires)
 
 
 class CityCollection:
-    def __init__(self, list_cities: list):
+
+
+    def __init__(self, list_cities: list, country: list, total_attendees: int):
         self.cities = list_cities
+        self.countries = country
+        self.total_attendees = total_attendees
 
     def countries(self) -> List[str]:
-        raise NotImplementedError
+        return self.countries()
 
     def total_attendees(self) -> int:
-        raise NotImplementedError
+        return self.total_attendees()
 
     def total_distance_travel_to(self, city: City) -> float:
         raise NotImplementedError
@@ -70,6 +75,18 @@ class CityCollection:
         raise NotImplementedError
 
 
-list_of_cities = ['zurich', 'san_francisco']
-city_collection =CityCollection(list_of_cities)
-print(city_collection.cities == list_of_cities)
+# list_of_cities = ['zurich', 'san_francisco']
+# city_collection =CityCollection(list_of_cities)
+# print(city_collection.cities == list_of_cities)
+
+
+
+
+
+
+Algiers = City('Algiers', 'Algeria', 1, 28.0000272, 2.9999825)
+
+Buenos_Aires = City('Buenos_Aires', 'Argentina', 5, -34.6075616, -58.437076)
+
+
+print(Algiers.distance_to(Buenos_Aires))
